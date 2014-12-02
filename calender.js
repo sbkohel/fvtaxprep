f85bfba98f371265b260f1ca97f23a7a1e21edd4
steflk84 <steflk84@uwosh.edu>
2014-12-02 17:26:37 -0600

    js that runs the calender

---
+++ calender.js
@@ -0,0 +1,84 @@
+	var dp = new DayPilot.Calendar("dp");
+		dp.init();
+
+var dp = new DayPilot.Calendar("dp");
+dp.cssClassPrefix = "calendar_white";
+dp.init();
+
+		dp.viewType = "Week";
+		dp.update();
+
+function loadEvents() {
+    DayPilot.request("backend_events.php", function(result) {
+        var data = eval("(" + result.responseText + ")");
+        for(var i = 0; i < data.length; i++) {
+            var e = new DayPilot.Event(data[i]);                
+            dp.events.add(e);
+        }
+    });
+}
+
+dp.onEventMoved = function (args) {
+    DayPilot.request(
+        "backend_move.php", 
+        function(req) { // success
+            var response = eval("(" + req.responseText + ")");
+            if (response && response.result) {
+                dp.message("Moved: " + response.message);
+            }
+        },
+        args,
+        function(req) {  // error
+            dp.message("Saving failed");
+        }
+    );        
+};
+
+dp.onEventResized = function (args) {
+    DayPilot.request(
+        "backend_resize.php", 
+        function(req) { // success
+            var response = eval("(" + req.responseText + ")");
+            if (response && response.result) {
+                dp.message("Resized: " + response.message);
+            }
+        },
+        args,
+        function(req) {  // error
+            dp.message("Saving failed");
+        }
+    );    
+};
+dp.onTimeRangeSelected = function (args) {
+    var name = prompt("New event name:", "Event");
+    dp.clearSelection();
+    if (!name) return;
+    var e = new DayPilot.Event({
+        start: args.start,
+        end: args.end,
+        id: DayPilot.guid(),
+        text: name
+    });
+    dp.events.add(e);
+
+    args.text = name;
+
+    DayPilot.request(
+        "backend_create.php", 
+        function(req) { // success
+            var response = eval("(" + req.responseText + ")");
+            if (response && response.result) {
+                dp.message("Created: " + response.message);
+            }
+        },
+        args,
+        function(req) {  // error
+            dp.message("Saving failed");
+        }
+    ); 
+};
+dp.onBeforeCellRender = function(args) {
+    if (args.cell.start.getDayOfWeek() === 6 || args.cell.start.getDayOfWeek() === 0) {
+        args.cell.backColor = "#eee";
+    }
+};
