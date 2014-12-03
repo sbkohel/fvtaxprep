0853cdf45ac8920f801d4d4723b3e7685ef6b591
Vanlab18 <Vanlab18@uwosh.edu>
2014-12-02 19:31:46 -0600

    upload file and download file work in progress

--- employeemain.php
+++ employeemain.php
@@ -22,15 +22,33 @@
         <div class="row">
             <div class = "col-lg-8 col-lg-offset-2 deduction">
                 <table class="table table-bordered table-striped">  
-                  <tr><button id = "upload">Upload PDF</button></tr>
+                  <tr>
+			<form action="upload.php" method="post" enctype="multipart/form-data">
+				<table width="350" border="0" cellpadding="1" cellspacing="1" class="box">
+					<tr> 
+						<td width="246">
+							<input type="hidden" name="MAX_FILE_SIZE" value="2000000">
+							<input name="userfile" type="file" id="userfile"> 
+						</td>
+						<td width="80"><input name="upload" type="submit" class="box" id="upload" value=" Upload "></td>
+					</tr>
+				</table>
+			</form>
+			<?php
+			$db = new PDO("mysql:dbname=vanlab18;host=localhost","vanlab18","Mountain123");
+			$result = $db->query("SELECT id, name FROM upload");
+			foreach($result as $row){
+					$ran = $row['id'];
+					$name = $row['name'];
+					print "<a href=\"download.php?id=". $ran ."\">" . $name . "</a><br/>";
+				}
+			?>
+                  </tr>
                   <tr><button id = "appointments">View Appointments</button></tr>
                   <tr><button id = "delete">Delete Appointments</button> </tr>
                     <h1>
                     Calender goes here
                     </h1>
-<?php
-
-?>
                 </table> 
             <div>
         </div>
@@ -39,6 +57,6 @@
         </p>
 	</div>
 	<script src=>"js/bootstrap.min.js"></script>
-  
 </body>
 </html>
+
