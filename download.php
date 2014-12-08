4895904e379689e9870f5f0a765437f4df109449
Vanlab18 <Vanlab18@uwosh.edu>
2014-12-07 22:17:04 -0600

    Update download.php

--- download.php
+++ download.php
@@ -1,19 +1,17 @@
 <?php
-if(isset($_GET['id'])) 
-{
-// if id is set then get the file with the id from database
-$id    = $_GET['id'];
-$db = new PDO("mysql:dbname=vanlab18;host=localhost","vanlab18","Mountain123");
-$result = $db->query("SELECT name, type, size, content FROM upload WHERE id = '$id'");
-foreach($result as $row){
-      // Print headers
-     header("Content-Type: ". $row['type']);
-     header("Content-Length: ". $row['size']);
-     header("Content-Disposition: attachment; filename=". $row['name']);
- 
-     // Print data
-     echo $row['content'];	
-	}
-}
+$file = basename($_POST['images']);
+$file = 'file_uploads/'.$file;
 
+if(!$file){ // file does not exist
+    die('file not found');
+} else {
+    header("Cache-Control: public");
+    header("Content-Description: File Transfer");
+    header("Content-Disposition: attachment; filename=$file");
+    header("Content-Type: application/zip");
+    header("Content-Transfer-Encoding: binary");
+
+    // read the file from disk
+    readfile($file);
+}
 ?>
