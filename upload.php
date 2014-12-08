a961f4da1f47c06cc1d95417311e1db4e975a144
Vanlab18 <Vanlab18@uwosh.edu>
2014-12-07 22:17:32 -0600

    Update upload.php

--- upload.php
+++ upload.php
@@ -1,26 +1,39 @@
 <?php
-// Check if a file has been uploaded
-if(isset($_POST['upload']) && $_FILES['userfile']['size'] > 0) {
-    // Make sure the file was sent without errors
-        $fileName = $_FILES['userfile']['name'];
-	$tmpName  = $_FILES["file"]["tmp_name"];
-	$fileSize = $_FILES['userfile']['size'];
-	$fileType = $_FILES['userfile']['type'];
-
-	$fp      = fopen($tmpName, 'r');
-	$content = fread($fp, filesize($tmpName));
-	$content = addslashes($content);
-	fclose($fp);
-
-	if(!get_magic_quotes_gpc())
-	{
- 	   $fileName = addslashes($fileName);
-	}
-       $db = new PDO("mysql:dbname=vanlab18;host=localhost","vanlab18","Mountain123");
-
- 
-        // Create the SQL query
-        $db->query("INSERT INTO upload (name, type, size, content) VALUES ('$fileName', '$tmpName', '$fileSize', '$fileType')");
+$target_dir = "file_uploads/";
+$target_file = $target_dir. basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1;
+$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+// Check if image file is a actual image or fake image
+if(isset($_POST["submit"])) {
+    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+    if($check !== false) {
+        echo "File is an image - " . $check["mime"] . ".";
+        $uploadOk = 1;
+    } else {
+        echo "File is not an image.";
+        $uploadOk = 0;
+    }
+}
+// Check if file already exists
+if (file_exists($target_file)) {
+    echo "Sorry, file already exists.";
+    $uploadOk = 0;
+}
+// Check file size
+if ($_FILES["fileToUpload"]["size"] > 5000000) {
+    echo "Sorry, your file is too large.";
+    $uploadOk = 0;
+}
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+    echo "Sorry, your file was not uploaded.";
+// if everything is ok, try to upload file
+} else {
+    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+		header('Location: employeemain.php'); 
+    } else {
+        echo "Sorry, there was an error uploading your file.";
+    }
 }
 ?>
- 
